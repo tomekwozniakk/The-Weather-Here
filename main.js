c5f06be761fef810494658c32c84cf791207e92b
Tomasz Wozniak <tomekwozniakk@gmail.com>
2023-05-23 10:18:53 +0200

    fixing bug when no air quality data available

--- main.js
+++ main.js
@@ -5,8 +5,8 @@ require("dotenv").config();
 
 
 const app = express();
-
-app.listen(3000, () => console.log("Listening at 3000..."));
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Starting server on ${port}`));
 app.use(express.static("public"));
 app.use(express.json({ limit: "1mb" }));
 
--- public/app.js
+++ public/app.js
@@ -1,4 +1,5 @@
 let lat, lon;
+
 function scale(number, inMin, inMax, outMin, outMax) {
   return ((number - inMin) * (outMax - outMin)) / (inMax - inMin) + outMin;
 }
@@ -17,15 +18,9 @@ if ("geolocation" in navigator) {
     console.log(json);
 
     const weather_data = json.weather;
-    const aq_data = json.aq.results[0];
-    const pm25 = aq_data.measurements.filter(
-      (result) => result.parameter === "pm25"
-    );
-    console.log(pm25);
-    const aq_city = aq_data.city;
-    const aq_location = aq_data.coordinates;
-    const temperature = Math.round(weather_data.main.temp);
-    const wind = Math.round(weather_data.wind.speed);
+
+    const temperature = Math.round(weather_data.main.temp) || '-';
+    const wind = Math.round(weather_data.wind.speed) || '-';
     const wind_gust = Math.round(weather_data.wind.gust) || "-";
 
     document.getElementById("summary").textContent =
@@ -34,18 +29,59 @@ if ("geolocation" in navigator) {
     document.getElementById(
       "temperature--big"
     ).textContent = `${temperature}°C`;
-    document.getElementById("aq_value").textContent = pm25[0].value;
-    document.getElementById("aq_units").textContent = pm25[0].unit;
-    document.getElementById("aq_date").textContent = new Date(
-      pm25[0].lastUpdated
-    ).toLocaleString();
-    if (aq_city) {
-      document.getElementById("aq_location").textContent = aq_city;
+
+    const aq_data = json.aq.results[0];
+
+    if (aq_data) {
+      const pm25 = aq_data.measurements.filter(
+        (result) => result.parameter === "pm25"
+      );
+
+      const aq_city = aq_data.city;
+      const aq_location = aq_data.coordinates;
+
+      document.getElementById("aq_value").textContent = pm25[0].value;
+      document.getElementById("aq_units").textContent = pm25[0].unit;
+      document.getElementById("aq_date").textContent = new Date(
+        pm25[0].lastUpdated
+      ).toLocaleString();
+      if (aq_city) {
+        document.getElementById("aq_location").textContent = aq_city;
+      } else {
+        document.getElementById(
+          "aq_location"
+        ).textContent = `${aq_location.latitude}, ${aq_location.longitude}`;
+      }
+
+      let aq_index;
+      let aq_class;
+      if (pm25[0].value <= 13) {
+        aq_index = "Very Good";
+        aq_class = "very_good";
+      } else if (pm25[0].value > 13 && pm25[0].value <= 35) {
+        aq_index = "Good";
+        aq_class = "good";
+      } else if (pm25[0].value > 35 && pm25[0].value <= 55) {
+        aq_index = "Moderate";
+        aq_class = "moderate";
+      } else if (pm25[0].value > 55 && pm25[0].value <= 75) {
+        aq_index = "Sufficient";
+        aq_class = "sufficient";
+      } else if (pm25[0].value > 75 && pm25[0].value <= 110) {
+        aq_index = "Bad";
+        aq_class = "bad";
+      } else if (pm25[0].value > 110) {
+        aq_index = "Very Bad";
+        aq_class = "very_bad";
+      }
+      document.getElementById("air--quality").textContent = aq_index;
+      document.getElementById("parameter--aq").classList.add(aq_class);
     } else {
-      document.getElementById(
-        "aq_location"
-      ).textContent = `${aq_location.latitude}, ${aq_location.longitude}`;
+      document.getElementById("air-description").textContent = 'NO AIR QUALITY READING AVAILABLE';
+      document.getElementById("air--quality").textContent = '-';
     }
+
+
     document.getElementById(
       "weather--image"
     ).src = `https://openweathermap.org/img/wn/${weather_data.weather[0].icon}@4x.png`;
@@ -70,31 +106,14 @@ if ("geolocation" in navigator) {
       "humidity"
     ).textContent = `${weather_data.main.humidity}%`;
 
-    let aq_index;
-    let aq_class;
-    if (pm25[0].value <= 13) {
-      aq_index = "Very Good";
-      aq_class = "very_good";
-    } else if (pm25[0].value > 13 && pm25[0].value <= 35) {
-      aq_index = "Good";
-      aq_class = "good";
-    } else if (pm25[0].value > 35 && pm25[0].value <= 55) {
-      aq_index = "Moderate";
-      aq_class = "moderate";
-    } else if (pm25[0].value > 55 && pm25[0].value <= 75) {
-      aq_index = "Sufficient";
-      aq_class = "sufficient";
-    } else if (pm25[0].value > 75 && pm25[0].value <= 110) {
-      aq_index = "Bad";
-      aq_class = "bad";
-    } else if (pm25[0].value > 110) {
-      aq_index = "Very Bad";
-      aq_class = "very_bad";
-    }
-    document.getElementById("air--quality").textContent = aq_index;
-    document.getElementById("parameter--aq").classList.add(aq_class);
 
-    const data = { lat, lon, weather_data, aq_data };
+
+    const data = {
+      lat,
+      lon,
+      weather_data,
+      aq_data
+    };
     const options = {
       method: "POST",
       headers: {
